b3fff16ffcca18a437ddac8869ca71583cf26e08
Anne-Cécile Mainier <annececile.mainier@gmail.com>
2026-06-27 11:53:48 +0100

    Refactor questions.py to use a Questions class with ask() and to_dict() methods

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from rich.console import Console
 from rich.panel import Panel
-from questions import ask_questions
+from questions import Questions
 from generator import generate_readme
 
 # Initialize Rich Console
@@ -9,9 +9,12 @@ console = Console()
 # Display Styled Welcome Text
 console.print(Panel.fit("[bold orchid]Welcome to my Python README file Generator![/bold orchid]"))
 
-answers = ask_questions()
-
-path = generate_readme(answers)
+# Create instance of class
+questions = Questions()
+# Call method to trigger prompt
+questions.ask()
+# Call method to get dictionary
+path = generate_readme(questions.to_dict())
 
 # Display Styled Sucess Text + link
 console.print(Panel.fit(f"[chartreuse3]Success! Your file has been created! Click [link=file://{path}]here[/link] to open it![/chartreuse3]"))
--- questions.py
+++ questions.py
@@ -1,22 +1,26 @@
 from InquirerPy import inquirer
 
-# Get user input with Inquirer
+# Get user input with Inquirer and in a class
+class Questions:
+    def ask(self):
+        self.title = inquirer.text(message="Project Title:").execute()
+        self.description = inquirer.text(message="Project description:").execute()
+        self.instructions = inquirer.text(message="Installation instructions:").execute()
+        self.usage = inquirer.text(message="Usage information:").execute()
+        self.license_type = inquirer.select(message="License", choices=["MIT", "Apache 2.0", "GPL 3.0"]).execute()
+        self.author = inquirer.text(message="Author name:").execute()
+        self.contact = inquirer.text(message="Contact information:").execute()
 
-def ask_questions():
-    title = inquirer.text(message="Project Title:").execute()
-    description = inquirer.text(message="Project description:").execute()
-    instructions = inquirer.text(message="Installation instructions:").execute()
-    usage = inquirer.text(message="Usage information:").execute()
-    license_type = inquirer.select(message="License", choices=["MIT", "Apache 2.0", "GPL 3.0"]).execute()
-    author = inquirer.text(message="Author name:").execute()
-    contact = inquirer.text(message="Contact information:").execute()
+        return self
 
-    return {
-        "title": title,
-        "description": description,
-        "instructions": instructions,
-        "usage": usage,
-        "license": license_type,
-        "author": author,
-        "contact": contact
+    # Changing class attributes to dictionary
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "description": self.description,
+            "instructions": self.instructions,
+            "usage": self.usage,
+            "license": self.license_type,
+            "author": self.author,
+            "contact": self.contact
     }
