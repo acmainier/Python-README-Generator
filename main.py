16b0ab224238d8402d83ec9e4edf9a777c9c9c38
Anne-Cécile Mainier <annececile.mainier@gmail.com>
2026-06-27 10:17:48 +0100

    Add Rich welcome banner and success message with clickable file link

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from InquirerPy import inquirer
+from rich.console import Console
+from rich.panel import Panel
+import os
+
+# Initialize Rich Console
+console = Console()
+
+# 1. Display Styled Text
+console.print(Panel.fit("[bold orchid]Welcome to my Python README file Generator![/bold orchid]"))
+
 
 # Get user input with Inquirer
 
@@ -10,7 +20,7 @@ license_type = inquirer.select(message="License", choices=["MIT", "Apache 2.0", 
 author = inquirer.text(message="Author name:").execute()
 contact = inquirer.text(message="Contact information:").execute()
 
-print(title, description, instructions, usage, license_type, author, contact)
+# Format input in Markdown
 
 content = f"""# {title}
 
@@ -30,7 +40,13 @@ content = f"""# {title}
 {author}
 
 ## Contact information:
-{contact}  """
+{contact}"""
+
+# Creation of README.md + addition of input
 
 with open("README.md", "w") as file:
         file.write(content)
+
+path = os.path.abspath("README.md")
+
+console.print(Panel.fit(f"[chartreuse3]Success! Your file has been created! Click [link=file://{path}]here[/link] to open it![/chartreuse3]"))
